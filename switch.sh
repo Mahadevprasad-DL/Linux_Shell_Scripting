20b2e411cab91511e39531da1c54f63f59b6ad50
Mahadevprasad D L <mahadevprasaddl@gmail.com>
2025-01-05 09:45:32 +0530

    Delete switch.sh

--- switch.sh
+++
@@ -1,9 +0,0 @@
-# Swtich#
-#!/bin/bash
-case "$NAMES" in
-   "girish") echo "This is my name" ;;
-   "Prasad") echo "This is my nick name" ;;
-   "Mahadev") echo "My second nick name" ;;
-   *) echo "No match found" ;;  
-esac
-
